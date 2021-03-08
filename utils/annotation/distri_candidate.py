97fbab1a5b3b0f227e08892990307f4470ab6318
mayixiao <mayx20@mails.tsinghua.edu.cn>
2021-03-08 15:39:12 +0800

    add candidate zipfile

--- utils/annotation/distri_candidate.py
+++ utils/annotation/distri_candidate.py
@@ -11,28 +11,58 @@ import re
 import numpy as np
 import json
 import argparse
+from shutil import copy
 from tqdm import tqdm
 
 parser = argparse.ArgumentParser(description="Help info.")
 parser.add_argument('--d', type=str, default='data/corpus/documents', help='Document dir path.')
-parser.add_argument('--q', type=str, default='data/query/query.json', help='Query path.')
-parser.add_argument('--w', type=str, default='/work/mayixiao/similar_case/类案数据集标注', help='Write path.')
+parser.add_argument('--q', type=str, default='data/prediction/combined_top100.json', help='Query path.') # query.json
+# parser.add_argument('--w', type=str, default='/work/mayixiao/similar_case/candidates', help='Write path.')
 
 args = parser.parse_args()
 
-with open(args.q, 'r') as g:
-    qs = g.readlines()
+# with open(args.q, 'r') as g:
+#     qs = g.readlines()
 
-for i in range(107):
-    temc = eval(qs[i])['candidate']
-    # print(len(temc))
-    j = 1
-    for cand in temc:
-        temqw = ''
-        with open(os.path.join(args.c, cand), 'r') as f:
-            temqw = json.load(f)['qw'].replace(' ', '\n')
-        with open(os.path.join(args.w, str(i+1),str(i+1)+'-'+str(j)+'.txt'),'w') as f:
-            f.write(temqw)
-        j += 1
-    # with open(os.path.join(WROOT,str(i+1),'q.txt'),'w') as f:
-    #     f.write(temq)
+# for i in range(107):
+#     temc = eval(qs[i])['candidate']
+#     # print(len(temc))
+#     j = 1
+#     for cand in temc:
+#         temqw = ''
+#         with open(os.path.join(args.c, cand), 'r') as f:
+#             temqw = json.load(f)['qw'].replace(' ', '\n')
+#         with open(os.path.join(args.w, str(i+1),str(i+1)+'-'+str(j)+'.txt'),'w') as f:
+#             f.write(temqw)
+#         j += 1
+#     # with open(os.path.join(WROOT,str(i+1),'q.txt'),'w') as f:
+#     #     f.write(temq)
+
+root = '/work/yangjun/LAW/preprocess_new_data/feature_data'
+with open('data/corpus/document_path.json', 'r') as f:
+    jsfile = json.load(f)
+
+paths = []
+for i in jsfile['single']:
+    paths.append(i)
+
+for j in jsfile['retrial']:
+    for i in j:
+        paths.append(i)
+
+print(len(paths))
+
+with open(args.q, 'r') as f:
+    qs = json.load(f)
+
+for key in tqdm(list(qs.keys())):
+    keydir = os.path.join(args.w, key)
+    if not os.path.exists(keydir):
+        os.mkdir(keydir)
+        for cand_num in qs[key]:
+            cpath = os.path.join(root, paths[cand_num]) 
+            copy(cpath, os.path.join(keydir, str(cand_num)+'.json'))
+
+
+
+            
