507620f7edcd81545ba00f782e35b3e920a1f154
mayixiao <mayx20@mails.tsinghua.edu.cn>
2021-03-15 09:46:31 +0800

    update q101-107 labels

--- utils/annotation/read_excel_label.py
+++ utils/annotation/read_excel_label.py
@@ -14,7 +14,7 @@ from tqdm import tqdm
 import numpy as np
 
 parser = argparse.ArgumentParser(description="Help info.")
-parser.add_argument('--edir', type=str, default='/work/mayixiao/similar_case/LeCaRD/label', help='Excel dir path.')
+parser.add_argument('--edir', type=str, default='/work/mayixiao/similar_case/LeCaRD/excel', help='Excel dir path.')
 parser.add_argument('--predir', type=str, default='data/prediction', help='Prediction dir path.')
 parser.add_argument('--w', type=str, default='data/label/label.json', help='Write path.')
 
@@ -25,8 +25,10 @@ files = [['part1.xlsx', 'part2.xlsx', 'part3--程睿标注.xlsx'],
                 ['1-辛佳东-法大.xlsx', '2_part2(1).xlsx', '3-中共中央党校-郭坤旭.xlsx']]
 bert_files = ['/work/mayixiao/coliee_2020/coliee20_pytorch_worker/result/LeCaRD1.json', '/work/mayixiao/coliee_2020/                coliee20_pytorch_worker/result/LeCaRD2.json', '/work/mayixiao/coliee_2020/coliee20_pytorch_worker/result/LeCaRD3.json', '/work/mayixiao/coliee_2020/coliee20_pytorch_worker/result/LeCaRD4.json']
 
+# files = ['part3.xlsx', 'part3(1).xlsx', '最后七个.xlsx']
+
 wjson = []
-for i in range(3):
+for i in range(3):      
     temlist = []
     for j in range(3):
         data = xlrd.open_workbook(os.path.join(args.edir, files[i][j]))
@@ -38,9 +40,20 @@ for i in range(3):
             temlist.append(table.col_values(2)[1:])
     wjson.append(temlist)
     print(len(temlist))
+    
+# wjson = []
+# for i in range(3):
+#     temlist = []
+#     data = xlrd.open_workbook(os.path.join(args.edir, files[i]))
+#     for k in range(101,108):
+#         table = data.sheet_by_name(str(k))
+#         temlist.append(table.col_values(2)[1:])
+#     wjson.append(temlist)
+
+# print(wjson)
 
 avglist = []
-for i in range(100):
+for i in range(100): # 7
     temlist = []
     for j in range(30):
         tem = 0
@@ -57,6 +70,8 @@ for i in range(100):
     
     avglist.append(temlist)
 wjson.append(avglist)
+
+# print(avglist)
 
 # read predictions from BERT 
 with open(os.path.join(args.predir, 'combined_top100.json') , 'r') as f:
