07a7f3add46457e914fc5308dbf939f4c71406fa
mayixiao <mayx20@mails.tsinghua.edu.cn>
2021-07-06 11:35:50 +0800

    update label_top30 & metrics

--- metrics.py
+++ metrics.py
@@ -12,7 +12,7 @@ import json
 import math
 import functools
 import argparse
-from sklearn.metrics import ndcg_score
+# from sklearn.metrics import ndcg_score
 from tqdm import tqdm
 
 def kappa(testData, k): #testData表示要计算的数据，k表示数据矩阵的是k*k的
@@ -90,19 +90,22 @@ def load_file(args):
     for key in list(combdic.keys())[:100]:
         tdic[key].reverse()
         bdic[key].reverse()
+
+    with open('/work/mayixiao/lawformer/lawformer_top30.json', 'r') as f:
+        lawformer_dic = json.load(f)
     
-    return avglist, bertdics, combdic, tdic, ldic, bdic
+    return avglist, bertdics, combdic, tdic, ldic, bdic, lawformer_dic
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Help info:")
     parser.add_argument('--m', type=str, choices= ['NDCG', 'P', 'MAP', 'KAPPA'], default='NDCG', help='Metric.')
-    parser.add_argument('--label', type=str, default='data/label/label_top30.json', help='Label file path.')
+    parser.add_argument('--label', type=str, default='data/label/label_top30_dict.json', help='Label file path.')
     parser.add_argument('--pred', type=str, default='data/prediction', help='Prediction dir path.')
     parser.add_argument('--q', type=str, choices= ['all', 'common', 'controversial', 'test'], default='all', help='query set')
 
     args = parser.parse_args()
 
-    avglist, bertdics, combdic, tdic, ldic, bdic = load_file(args)
+    avglist, bertdics, combdic, tdic, ldic, bdic, lawformer_dic = load_file(args)
 
     dics = [bdic, tdic, ldic]
     if args.q == 'all':
@@ -113,8 +116,9 @@ if __name__ == "__main__":
         keys = list(combdic.keys())[77:100]
     elif args.q == 'test':
         keys = [i for i in list(combdic.keys())[:100] if list(combdic.keys())[:100].index(i) % 5 == 0]
-        dics = [bdic, tdic, ldic, eval(blines[1])]
-        # dics = [eval(blines[0]),eval(blines[1]),eval(blines[2]),eval(blines[3])]
+        # dics = [bdic, tdic, ldic, bertdics[1]]
+        # dics = [bertdics[1]]
+        dics = [lawformer_dic]
     
     if args.m == 'NDCG':
         topK_list = [10, 20, 30]
@@ -125,7 +129,8 @@ if __name__ == "__main__":
             for redic in dics:
                 sndcg = 0.0
                 for key in keys:
-                    rawranks = [4 - avglist[key][list(combdic[key][:30]).index(i)] for i in redic[key] if i in list(combdic[key][:30])]
+                    # rawranks = [4 - avglist[key][list(combdic[key][:30]).index(i)] for i in redic[key] if i in list(combdic[key][:30])]
+                    rawranks = [avglist[key][str(i)] for i in redic[key] if i in list(combdic[key][:30])]
                     # print(rawranks) 
                     ranks = rawranks + [0]*(30-len(rawranks))
                     if sum(ranks) != 0:
@@ -144,7 +149,8 @@ if __name__ == "__main__":
                 sp = 0.0
                 for key in keys:
                     ranks = [i for i in rdic[key] if i in list(combdic[key][:30])] 
-                    sp += float(len([j for j in ranks[:topK] if avglist[key][list(combdic[key][:30]).index(j)] == 1])/topK)
+                    # sp += float(len([j for j in ranks[:topK] if avglist[key][list(combdic[key][:30]).index(j)] == 1])/topK)
+                    sp += float(len([j for j in ranks[:topK] if avglist[key][str(j)] == 3])/topK)
                 temK_list.append(sp/len(keys))
             sp_list.append(temK_list)
         print(sp_list)
@@ -155,10 +161,12 @@ if __name__ == "__main__":
             smap = 0.0
             for key in keys:
                 ranks = [i for i in rdic[key] if i in list(combdic[key][:30])] 
-                rels = [ranks.index(i) for i in ranks if avglist[key][list(combdic[key][:30]).index(i)] == 1]
+                # rels = [ranks.index(i) for i in ranks if avglist[key][list(combdic[key][:30]).index(i)] == 1]
+                rels = [ranks.index(i) for i in ranks if avglist[key][str(i)] == 3]
                 tem_map = 0.0
                 for rel_rank in rels:
-                    tem_map += float(len([j for j in ranks[:rel_rank+1] if avglist[key][list(combdic[key][:30]).index(j)] == 1])/(rel_rank+1))
+                    # tem_map += float(len([j for j in ranks[:rel_rank+1] if avglist[key][list(combdic[key][:30]).index(j)] == 1])/(rel_rank+1))
+                    tem_map += float(len([j for j in ranks[:rel_rank+1] if avglist[key][str(j)] == 3])/(rel_rank+1))
                 if len(rels) > 0:
                     smap += tem_map / len(rels)
             map_list.append(smap/len(keys))
