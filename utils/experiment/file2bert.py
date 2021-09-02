5220b412c9822167f991bb6a1d5544ba041f2db7
mayixiao <mayx20@mails.tsinghua.edu.cn>
2021-09-02 21:08:38 +0800

    update file2bert

--- utils/experiment/file2bert.py
+++ utils/experiment/file2bert.py
@@ -17,22 +17,37 @@ sys.path.append('/work/mayixiao/www22/')
 from pre_ajjbqk import process_ajjbqk
 
 parser = argparse.ArgumentParser(description="Help info.")
-parser.add_argument('--l', type=str, default='/work/mayixiao/similar_case/202006/data/label/label_top30_dict_2.json', help='Label file path.')
-parser.add_argument('--q', type=str, default='/work/mayixiao/similar_case/202006/data/query/query2_final.json', help='Query file path.')
-parser.add_argument('--d', type=str, default='/work/mayixiao/similar_case/202006/data/candidates_2', help='Document dir path.')
 parser.add_argument('--short', type=bool, default=False, help='if pre ajjbqk.')
-parser.add_argument('--w', type=str, default='/work/mayixiao/www22/train_2', help='Write file path.')
+
+parser.add_argument('--l', type=str, default='/work/mayixiao/similar_case/LeCaRD/LeCaRD_github/data/label/label_top30_dict.json', help='Label file path.')
+parser.add_argument('--q', type=str, default='/work/mayixiao/similar_case/LeCaRD/LeCaRD_github/data/query/query.json', help='Query file path.')
+parser.add_argument('--d', type=str, default='/work/mayixiao/similar_case/candidates', help='Document dir path.')
+parser.add_argument('--w', type=str, default='/work/mayixiao/www22/test', help='Write file path.')
+cpfxgc_dic = json.load(open('/work/mayixiao/www22/extracted_cpfxgc.json', 'r'))
+
+# cpfxgc_dic = json.load(open('/work/mayixiao/www22/extracted_cpfxgc_2.json', 'r'))
+# parser.add_argument('--l', type=str, default='/work/mayixiao/similar_case/202006/data/label/label_top30_dict_2.json', help='Label file path.')
+# parser.add_argument('--q', type=str, default='/work/mayixiao/similar_case/202006/data/query/query2_final.json', help='Query file path.')
+# parser.add_argument('--d', type=str, default='/work/mayixiao/similar_case/202006/data/candidates_2', help='Document dir path.')
+# parser.add_argument('--w', type=str, default='/work/mayixiao/www22/train_2', help='Write file path.')
+# cpfxgc_dic = json.load(open('/work/mayixiao/www22/extracted_cpfxgc_2.json', 'r'))
 
 args = parser.parse_args()
 
 w_list = []
 with open(args.q, 'r') as f:
     lines = f.readlines()
+    if args.w.split('/')[-1] == 'train':
+        lines = [line for line in lines if (lines.index(line)%5 != 0 or lines.index(line)>=100)]
+    elif args.w.split('/')[-1] == 'test':
+        lines = [line for line in lines if (lines.index(line)%5 == 0 and lines.index(line)<100)]
+    else:
+        raise NotImplementedError
 
 with open(args.l, 'r') as f:
     labels = json.load(f)
 
-cpfxgc_dic = json.load(open('/work/mayixiao/www22/extracted_cpfxgc_2.json', 'r'))
+
 raw_c_list = open('/work/mayixiao/similar_case/LeCaRD/LeCaRD_github/data/others/criminal charges.txt', 'r').readlines()
 c_list = [c[:-1] for c in raw_c_list[:-1]]
 c_list.append(raw_c_list[-1])
@@ -42,13 +57,19 @@ def make_data(dic, qid, cid):
     tem['guid'] = qid + '_' + cid
     tem['text_a'] = dic['q']
     c_dic = json.load(open(os.path.join(args.d, qid, cid+'.json'), 'r'))
-    c_doc = c_dic['pjjg']
+    if 'pjjg' in c_dic:
+        c_doc = c_dic['pjjg']
+    else:
+        c_doc = c_dic['ajjbqk']
     if args.short:
         tem['text_b'] = process_ajjbqk(c_dic['ajjbqk'])
     else:
         tem['text_b'] = c_dic['ajjbqk']
     
-    tem['text_c'] = cpfxgc_dic[cid]
+    if cid in cpfxgc_dic:
+        tem['text_c'] = cpfxgc_dic[cid]
+    else: 
+        tem['text_c'] = []
     tem['c_a'] = dic['crime']
     tem['c_b'] = []
     for crime in c_list:
@@ -97,8 +118,9 @@ else:
     tail = '.json'
 
 # with open(args.w + tail, 'w') as f:
-#     for line in w_list:
-#         json.dump(line,f, ensure_ascii=False)
+#     for line in w_list[:-1]:
+#         json.dump(line, f, ensure_ascii=False)
 #         f.write('\n')
+#     json.dump(w_list[-1], f, ensure_ascii=False)
 
 print(max_len)
